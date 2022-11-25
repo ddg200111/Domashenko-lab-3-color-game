2775d84421e3ea42df2b50a435a9ccf42efa9214
ddg200111 <ddg2001@ukr.net>
2022-11-25 13:55:12 +0200

    domashenko_lab3

---
+++ app/src/main/java/com/example/lab3/result.java
@@ -0,0 +1,49 @@
+package com.example.lab3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class result extends AppCompatActivity {
+    Button again, exit;
+    TextView result, points;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        again=findViewById(R.id.button3);
+        exit=findViewById(R.id.button5);
+        result=findViewById(R.id.textView4);
+        points=findViewById(R.id.textView7);
+        addListenerOnButton();
+        points.setText(getIntent().getStringExtra("key"));
+    }
+
+    void addListenerOnButton(){
+        again.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Intent intent = new Intent(result.this, MainActivity.class);
+                        finish();
+                        startActivity(intent);
+                    }
+                }
+        );
+        exit.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        finish();
+                    }
+                }
+        );
+    }
+
+}
